6e14ac8c7a27081dc654f5ad91cefcb3081d4003
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-09-16 13:31:04 +0600

    dynamic laout and dynamic page componets

--- src/app/components/UserCard.jsx
+++ src/app/components/UserCard.jsx
@@ -1,13 +1,16 @@
+import Link from 'next/link';
 import React from 'react';
 
-const UserCardPage = ({userData}) => {
+const UserCardPage = ({ userData }) => {
     return (
         <div className="card card-dash bg-base-100 w-96">
             <div className="card-body">
                 <h2 className="card-title">{userData.name}</h2>
                 <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <Link href={`/users/${userData.id}`}>
+                        <button className="btn btn-primary">See User Details</button>
+                    </Link>
                 </div>
             </div>
         </div>
